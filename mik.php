dea4ad8d60c34b981da36bc30a9ce41c5aa27ed3
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-03-25 13:34:44 -0700

    Initial setup of composer and autoloading (with gitignore to ignore vendor code).

---
+++ mik.php
@@ -0,0 +1,10 @@
+<?php
+// mik.php
+/**
+ * Main Move to Islandora Kit script.
+ **/
+
+// Use composer to load vendor and project classes.
+require 'vendor/autoload.php';
+
+echo "Welcome the Move to Islandora Kit project.\n";
