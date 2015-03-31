475134977f3a5aedf1d0e41a7de1ca2dd5df703c
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-03-31 12:02:53 -0700

    Intial work on configuration and metadata classes.

---
+++ src/config/CdmConfig.php
@@ -0,0 +1,52 @@
+<?php
+// src/config/CdmConfig.php
+
+namespace mik\config;
+
+/**
+ *  Configuration for ContentDM source and CDM->MODS mapping in CSV.
+ */
+class CdmConfig extends Config
+{
+    /**
+     * @var string $wsULR URL to the CONTENTdm web services API
+     */
+    public $wsUrl;
+
+    /**
+     * @var string $mappingCSVpath Path to the csv file that contains
+     * the CONTENTdm to MODS mappings.
+     */
+    public $mappingCSVpath;
+
+    /**
+     * @var string $outputBaseDir The directory where item-level directories 
+     * will be created.
+     */
+    //public $outputBaseDir;
+
+    /**
+     * @var 1/0 $includeMigratedFromUri - Include the migrated from uri
+     * into your generated metadata (e.g., MODS)
+     */
+    public $includeMigratedFromUri;
+
+    /**
+     * Create a new Config Instance
+     * @param $configPath - path to configuraiton file.
+     */
+    public function __construct($configPath)
+    {
+        // Call Config.php constructor to include $settings and $outputBaseDir.
+        parent::__construct($configPath);
+
+        $this->wsUrl = $this->settings['API']['ws_url'];
+        //echo $this->wsUrl . "\n";
+
+        $this->mappingCSVpath = $this->settings['INPUT']['mapping_csv_path'];
+        //echo $this->mappingCSVpath . "\n";
+
+        $this->includeMigratedFromUri = $this->settings['METADATA']['include_migrated_from_uri'];
+        //echo $this->includeMigratedFromUri . "\n";
+    }
+}
---
+++ src/config/Config.php
@@ -0,0 +1,42 @@
+<?php
+// src/config/Config.php
+
+namespace mik\config;
+
+class Config
+{
+
+    /**
+     * @var Array that contains settings from parse_ini_file
+     */
+    public $settings;
+    
+    /**
+     * @var The directory where item-level directories will be created.
+     */
+    public $outputBaseDir;
+
+    /**
+     * Create a new Config Instance
+     * @param $configPath - path to configuraiton file.
+     */
+    public function __construct($configPath)
+    {
+        //echo $configPath;
+        $settings = parse_ini_file($configPath, true);
+        $this->settings = $settings;
+        $this->outputBaseDir = $settings['OUTPUT']['output_base_dir'];
+    }
+
+    /**
+    * Friendly welcome
+    *
+    * @param string $phrase Phrase to return
+    *
+    * @return string Returns the phrase passed in
+    */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
