318dbe79092e717f0fa87252e815b4c2eb653b18
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-04-14 14:16:11 -0700

    Added getChildrenPointers method - returns the pointers to the child items of compound item.

--- src/filegetters/Cdm.php
+++ src/filegetters/Cdm.php
@@ -39,4 +39,29 @@ class Cdm extends FileGetter
     {
         return array(1, 2, 3, 4, 5);
     }
+    
+    /**
+     * Gets a compound item's children pointers. $alias needs to include the leading '/'.
+     * @ToDo - clerify whether this method should be part of filegetters or fetchers.
+     */
+    public function getChildrenPointers($pointer)
+    {
+        $alias = $this->settings['alias'];
+        $ws_url = $this->settings['ws_url'];
+        $query_url = $ws_url . 'dmGetCompoundObjectInfo/' . $alias . '/' .  $pointer . '/json';
+        $item_structure = file_get_contents($query_url);
+        $item_structure = json_decode($item_structure, true);
+        
+        // @ToDo - deal with different item structures.
+        if (isset($item_structure['page'])) {
+            $children = $item_structure['page'];
+        } else {
+            return array();
+        }
+        $children_pointers = array();
+        foreach ($children as $child) {
+            $children_pointers[] = $child['pageptr'];
+        }
+        return $children_pointers;
+    }
 }
