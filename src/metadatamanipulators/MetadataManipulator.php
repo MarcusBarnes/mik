3f02bfeeb40409876a9b9059bf81e5a6358846b0
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-06-11 14:07:14 -0700

    Made MetadataManipulator an abstract class.

--- src/metadatamanipulators/MetadataManipulator.php
+++ src/metadatamanipulators/MetadataManipulator.php
@@ -3,13 +3,26 @@
 
 namespace mik\metadatamanipulators;
 
-class MetadataManipulator
+/**
+ * MetadataManipulator (abstract):
+ *    Methods related to manipulating metadata (typically in XML snippets).
+ *
+ *    Extend this abstract class with for specific implemenations.
+ *    For example, see metadatamanipulators/FilterModsTopics.php.
+ *
+ *    Note that methods marked as abstract must be defined in 
+ *    the extending class.
+ *
+ *    Abstract methods:
+ *        - manipulate
+ */
+abstract class MetadataManipulator
 {
     /**
      * @var array $settings - configuration settings from confugration class.
      */
     public $settings;
-      
+
     /**
      * Create a new Metadata Instance
      * @param array $settings configuration settings.
@@ -18,7 +31,17 @@ class MetadataManipulator
     {
         $this->settings = $settings;
     }
-    
+
+    /**
+     * General manipulate wrapper method.
+     *
+     * @param string $xmlsnippet XML snippet to be manipulated.
+     *
+     * @return string
+     *     Manipulated XML snippet
+     */
+    abstract public function manipulate($xmlsnippet);
+
     /**
     * Friendly welcome
     *
