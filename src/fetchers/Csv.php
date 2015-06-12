8f0ea30ec95a21f16b21f7765019969a0fe08ea8
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2015-06-11 17:20:04 -0700

    Created queryTotalRec and getItemInfo methods as required by abstract parent class until implementation specific details are added.

--- src/fetchers/Csv.php
+++ src/fetchers/Csv.php
@@ -40,4 +40,37 @@ class Csv extends Fetcher
 
         return $csv;
     }
+
+    /**
+     * Implements fetchers\Fetcher::queryTotalRec.
+     * 
+     * Returns the number of records under consideration.
+     *    For CSV, this will be the number of rows of data with a unique index.
+     *
+     * @return total number of records
+     *
+     * Note that extending classes must define this method.
+     */
+    public function queryTotalRec()
+    {
+        // @ToDo - implmentation specific details
+        return false;
+    }
+
+    /**
+     * Implements fetchers\Fetcher::getItemInfo
+     * Returns a hashed array or object containing a record's information.
+     *
+     * @param string $recordKey the unique record_key
+     *      For CONTENTdm, this will be the item pointer
+     *      For CSV, this will the the unique id assisgned to a row of data.
+     *
+     * @return array or object of record info.
+     */
+    public function getItemInfo($recordKey)
+    {
+        // @ToDo - implementation specific details.
+        return false;
+    }
+
 }
