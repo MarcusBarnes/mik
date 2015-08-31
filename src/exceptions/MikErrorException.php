99873c86c8f117c388b6670cd13f22a0bb6e4744
Marcus Barnes <mbarnes@sfu.ca>
2015-08-31 13:50:41 -0700

    Logging in MikErrorException class plus additional clean up.

--- src/exceptions/MikErrorException.php
+++ src/exceptions/MikErrorException.php
@@ -2,7 +2,8 @@
 
 namespace mik\exceptions;
 
-//use \Monolog\Logger;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 /**
  * Custom MIK Exception class.
@@ -10,12 +11,26 @@ namespace mik\exceptions;
 class MikErrorException extends \ErrorException
 {
 
-    public function __construct($message = null, $code = 0, $severity, $file, $line)
+    public function __construct($settings, $severity, $file, $line, $message = null, $code = 0)
     {
-        
+        $pathToLog = $settings['LOGGING']['path_to_log'];
+        $log = new Logger('ErrorException');
+        $logStreamHandler= new StreamHandler($pathToLog, Logger::ERROR);
+        $log->pushHandler($logStreamHandler);
+        $log->addError(
+            'ErrorException',
+            array(
+              'message' => $message,
+              'code' => $code,
+              'severity' => $severity,
+              'file' => $file,
+              'line' => $line
+            )
+        );
+
         //$this->message("MikErrorException class constructed" . PHP_EOL);
         //$previous = Exception::getPrevious;
-        parent::__construct($message, $code, $previous = NULL);
+        parent::__construct($message, $code, $previous = null);
         
     }
 
