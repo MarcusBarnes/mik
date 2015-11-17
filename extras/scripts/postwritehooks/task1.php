3e2a1aedf6b234446659cb5e8adbebeaa7c466f5
Mark Jordan <mjordan@sfu.ca>
2015-11-16 22:25:18 -0800

    Work on #92. Passing config in as JSON now works.

--- extras/scripts/postwritehooks/task1.php
+++ extras/scripts/postwritehooks/task1.php
@@ -1,10 +1,14 @@
 <?php
 
-$record_key = trim($argv[1]);
+/**
+ * All post-write hook scripts get the record key as the first parameter and
+ * a JSON representation of the MIK .ini file as the second.
+ */
 
-function shutdown() {
-  file_put_contents('/tmp/shutdown.log', "Task 1 for record key $record_key has shut down\n", FILE_APPEND);
-}
-register_shutdown_function('shutdown');
+$record_key = trim($argv[1]);
+$config_json = trim($argv[2]);
+$config = json_decode($config_json, true);
 
 file_put_contents('/tmp/task1.txt', "Record key from task1.php: $record_key\n", FILE_APPEND);
+file_put_contents('/tmp/task1.txt', "Output directory from MIK config: " . $config['WRITER']['output_directory'] . "\n", FILE_APPEND);
+file_put_contents('/tmp/task1.txt', $argv[0] . " has finished"  . "\n", FILE_APPEND);
--- extras/scripts/postwritehooks/task2.php
+++ extras/scripts/postwritehooks/task2.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ * All post-write hook scripts get the record key as the first parameter and
+ * a JSON representation of the MIK .ini file as the second.
+ */
+
 $record_key = trim($argv[1]);
+$config_json = trim($argv[2]);
+$config = json_decode($config_json, true);
 
-function shutdown() {
-  file_put_contents('/tmp/shutdown.log', "Task 2 for record key $record_key has shut down\n", FILE_APPEND);
+$path_to_file = $config['WRITER']['output_directory'] . DIRECTORY_SEPARATOR . $record_key . '.xml';
+
+file_put_contents('/tmp/task2.txt', $argv[0] . " processing file $path_to_file\n", FILE_APPEND);
+if (file_exists($path_to_file)) {
+    $file_size = filesize($path_to_file);
+    file_put_contents('/tmp/task2.txt', "File size: $file_size"  . "\n", FILE_APPEND);
 }
-register_shutdown_function('shutdown');
-
-file_put_contents('/tmp/task2.txt', "Record key from task2.php: $record_key\n", FILE_APPEND);
+else {
+    file_put_contents('/tmp/task2.txt', "File not found"  . "\n", FILE_APPEND);
+}
