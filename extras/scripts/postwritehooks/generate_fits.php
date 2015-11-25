155915b2388771e2a4bd830ca57f9d579fe3da84
Mark Jordan <mjordan@sfu.ca>
2015-11-24 21:38:56 -0800

    Some cleanup to generate_fits.php post write hook script.

--- extras/scripts/postwritehooks/generate_fits.php
+++ extras/scripts/postwritehooks/generate_fits.php
@@ -5,8 +5,9 @@
  * each child page of a newspaper issue or book.
  */
 
-// Relative to MIK, not this script.
-require 'vendor/autoload.php';
+/**
+ * You will need to adjust the variables in this next section.
+ */
 
 // The full path to the FITS executable on your system.
 $path_to_fits = '/home/mark/Documents/hacking/fits/fits-0.8.10/fits.sh';
@@ -14,18 +15,22 @@ $path_to_fits = '/home/mark/Documents/hacking/fits/fits-0.8.10/fits.sh';
 $fits_output_filename = 'TECHMD.xml';
 // Filename of the page-level TIFFs.
 $obj_filename = 'OBJ.tiff';
+// The CONTENTdm 'nick' for the field that contains the data used
+// to create the issue-level output directories.
 $item_info_field_for_issues = 'date';
 
+// Include all the components.
+require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use GuzzleHttp\Client;
 
-// Get the incoming parameters.
+// Get the parameters passed in from MIK.
 $record_key = trim($argv[1]);
 $children_record_keys_json = trim($argv[2]);
+$children_record_keys = json_decode($children_record_keys_json);
 $config_json = trim($argv[3]);
 $config = json_decode($config_json, true);
-$children_record_keys = json_decode($children_record_keys_json);
 
 // Set up logging.
 $log = new Logger('postwritehooks/generate_fits.php');
@@ -45,7 +50,7 @@ if (count($children_record_keys)) {
     $path_to_obj = $path_to_page_dir . DIRECTORY_SEPARATOR . $obj_filename;
     $path_to_fits_output = $path_to_page_dir . DIRECTORY_SEPARATOR . $fits_output_filename;
 
-    $cmd = "$path_to_fits -i $path_to_obj -x -o $path_to_fits_output";
+    $cmd = "$path_to_fits -i $path_to_obj -o $path_to_fits_output";
     exec($cmd, $output, $return_var);
     if ($return_var) {
       $log->addWarning("FITS output not generated", array('OBJ file' => $path_to_obj));
@@ -53,6 +58,24 @@ if (count($children_record_keys)) {
   }
 }
 
+/**
+ * Get the string identifying the issue-level directory where the
+ * page-level subdirectories are.
+ *
+ * @param string $record_key
+ *   The CONTENTdm object's pointer.
+ *
+ * @param string $item_info_field_for_issues
+ *   The CONTENTdm nick for the field that contains the string used
+ *   to create the issue-level directories in the MIK output.
+ *
+ * @param array $config
+ *   The MIK configuration settings.
+ *
+ * @return string|bool
+ *   The value of the CONTENTdm field specified in $item_info_field_for_issues,
+ *   or false if the field is not populated for this object.
+ */
 function get_issue_dir($record_key, $item_info_field_for_issues, $config) {
   // Use Guzzle to fetch the output of the call to GetParent
   // for the current object.
@@ -64,7 +87,7 @@ function get_issue_dir($record_key, $item_info_field_for_issues, $config) {
   } catch (Exception $e) {
     $this->log->addInfo("CdmNoParent",
       array('HTTP request error' => $e->getMessage()));
-    return true;
+    return false;
   }
   $body = $response->getBody();
   $item_info = json_decode($body, true);
