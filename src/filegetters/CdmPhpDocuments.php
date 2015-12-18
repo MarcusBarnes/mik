175a4016b02b4e327c6d5ae2de10a00a76fdb464
Mark Jordan <mjordan@sfu.ca>
2015-12-18 08:08:14 -0800

    Added default Guzzle http timeout property to CdmPhpDocuement filegetter.  Closes work on #3.

--- src/filegetters/CdmPhpDocuments.php
+++ src/filegetters/CdmPhpDocuments.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace mik\filegetters;
+
 use GuzzleHttp\Client;
+use mik\exceptions\MikErrorException;
+use Monolog\Logger;
 
 class CdmPhpDocuments extends FileGetter
 {
@@ -31,6 +34,19 @@ class CdmPhpDocuments extends FileGetter
         $this->alias = $this->settings['alias'];
         $this->temp_directory = (!isset($settings['FILE_GETTER']['temp_directory'])) ?
           '/tmp' : $settings['FILE_GETTER']['temp_directory'];
+
+        if (!isset($this->settings['http_timeout'])) {
+            // Seconds.
+            $this->settings['http_timeout'] = 60;
+        }
+
+        // Set up logger.
+        $this->pathToLog = $settings['LOGGING']['path_to_log'];
+        $this->log = new \Monolog\Logger('CdmPhpDocuments filegetter');
+        $this->logStreamHandler = new \Monolog\Handler\StreamHandler($this->pathToLog,
+            Logger::ERROR);
+        $this->log->pushHandler($this->logStreamHandler);
+
     }
 
     /**
@@ -81,16 +97,27 @@ class CdmPhpDocuments extends FileGetter
         // Create a new Guzzle client to fetch the PDF as a stream,
         // which will allow us to handle large PDF files.
         $client = new Client();
-        $response = $client->get($get_file_url, ['stream' => true]);
-        $body = $response->getBody();
-        while (!$body->eof()) {
-          file_put_contents($temp_file_path, $body->read(2048), FILE_APPEND);
+        try {
+            $response = $client->get($get_file_url, ['stream' => true,
+                'timeout' => $this->settings['http_timeout'],
+                'connect_timeout' => $this->settings['http_timeout']]
+            );
+            $body = $response->getBody();
+            while (!$body->eof()) {
+                file_put_contents($temp_file_path, $body->read(2048), FILE_APPEND);
+            }
+            if (file_exists($temp_file_path)) {
+                return $temp_file_path;
+            }
+            else {
+                return false;
+            }
         }
-        if (file_exists($temp_file_path)) {
-          return $temp_file_path;
-        }
-        else {
-          return false;
+        catch (RequestException $e) {
+            $this->log->addError("CdmSingleFile Guzzle error", array('HTTP request error' => $e->getRequest()));
+            if ($e->hasResponse()) {
+                $this->log->addError("CdmSingleFile Guzzle error", array('HTTP request response' => $e->getResponse()));
+            }
         }
     }
 }
