5cd2435efed965083221a98e1e68ebd1e99d7def
Mark Jordan <mjordan@sfu.ca>
2016-03-03 17:05:55 -0800

    Updated 'validate_mods_recursive.php script.

--- extras/scripts/validate_mods_recursive.php
+++ extras/scripts/validate_mods_recursive.php
@@ -3,16 +3,23 @@
 /**
  * Script to validate every .xml file under a directory agasint the
  * MODS 3.5 schema. Prints out paths to files that do not validate.
+ * Assumes schema file is in same directory as itself.
  *
+ * To validate all the .xml files in a directory and all descendent
+ * directories, run:
+ *   php validate_mods_recursive.php /path/to/dir
+ *
+ * You may want to modify $path_to_schema and $filenames_to_skip
+ * to suit your own needs.
  */
-
-$dir = trim($argv[1]);
 
 // List of filenames to not validate against the MODS schema.
 $filenames_to_skip = array('TECHMD');
 
 // Relative to this script.
 $path_to_schema = 'mods-3-5.xsd';
+
+$dir = trim($argv[1]);
 
 if (file_exists($path_to_schema)) {
 	  $schema_xml = file_get_contents($path_to_schema);
