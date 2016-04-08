5b29cf461a2c33f9ea8fcc5bc56895e938982a04
Mark Jordan <mjordan@sfu.ca>
2016-04-08 07:56:50 -0700

    Fixed broken indentation in generate_fits.php.

--- extras/scripts/postwritehooks/generate_fits.php
+++ extras/scripts/postwritehooks/generate_fits.php
@@ -47,81 +47,81 @@ $error_handler = new StreamHandler($path_to_error_log, Logger::WARNING);
 $error_log->pushHandler($error_handler);
 
 if (!file_exists($path_to_fits)) {
-  $error_log->addWarning("FITS executable cannot be found", array('Path' => $path_to_fits));
+    $error_log->addWarning("FITS executable cannot be found", array('Path' => $path_to_fits));
 }
 
 if ($config['FILE_GETTER']['class'] == 'CdmNewspapers') {
-	generate_fits_for_newspsper_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $item_info_field_for_issues, $record_key, $children_record_keys);
+    generate_fits_for_newspsper_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $item_info_field_for_issues, $record_key, $children_record_keys);
 }
 if ($config['FILE_GETTER']['class'] == 'CdmBooks') {
-	generate_fits_for_book_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $record_key, $children_record_keys);
+    generate_fits_for_book_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $record_key, $children_record_keys);
 }
 
 function generate_fits_for_newspsper_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $item_info_field_for_issues, $record_key, $children_record_keys) {
-	if (count($children_record_keys)) {
-		foreach ($children_record_keys as $child_record_key) {
-			if (!$issue_dir = get_issue_dir_name($record_key, $item_info_field_for_issues, $config)) {
-				$error_log->addWarning("FITS output not generated because issue directory not retrievable from CONTENTdm",
-					array('Issue dir' => $record_key));
-				exit;
-			}
-			$path_to_issue_dir = $config['WRITER']['output_directory'] . DIRECTORY_SEPARATOR . $issue_dir;
-			$page_dirs = glob($path_to_issue_dir . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
-			foreach ($page_dirs as $path_to_page_dir) {
-				$path_to_obj = $path_to_page_dir . DIRECTORY_SEPARATOR . $obj_filename;
-				if (file_exists($path_to_obj)) {
-					$path_to_fits_output = $path_to_page_dir . DIRECTORY_SEPARATOR . $fits_output_filename;
-					if (!file_exists($path_to_fits_output)) {
-						$cmd = "$path_to_fits -i $path_to_obj -xc -o $path_to_fits_output";
-						exec($cmd, $output, $return_var);
-						if ($return_var) {
-							$error_log->addWarning("FITS output not generated due to error with FITS",
-								array('FITS return value' => $return_var, 'OBJ file' => $path_to_obj));
-						}
-						else {
-							$info_log->addInfo("FITS output generated",
-								array('OBJ file' => $path_to_obj, 'FITS output' => $path_to_fits_output));
-						}
-					}
-				}
-				else {
-					$error_log->addWarning("FITS output not generated because OBJ file not found",
-						array('OBJ file' => $path_to_obj));
-				}
-			}
-		}
-	}
+    if (count($children_record_keys)) {
+        foreach ($children_record_keys as $child_record_key) {
+            if (!$issue_dir = get_issue_dir_name($record_key, $item_info_field_for_issues, $config)) {
+                $error_log->addWarning("FITS output not generated because issue directory not retrievable from CONTENTdm",
+                    array('Issue dir' => $record_key));
+                exit;
+            }
+            $path_to_issue_dir = $config['WRITER']['output_directory'] . DIRECTORY_SEPARATOR . $issue_dir;
+            $page_dirs = glob($path_to_issue_dir . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
+            foreach ($page_dirs as $path_to_page_dir) {
+                $path_to_obj = $path_to_page_dir . DIRECTORY_SEPARATOR . $obj_filename;
+                if (file_exists($path_to_obj)) {
+                    $path_to_fits_output = $path_to_page_dir . DIRECTORY_SEPARATOR . $fits_output_filename;
+                    if (!file_exists($path_to_fits_output)) {
+                        $cmd = "$path_to_fits -i $path_to_obj -xc -o $path_to_fits_output";
+                        exec($cmd, $output, $return_var);
+                        if ($return_var) {
+                            $error_log->addWarning("FITS output not generated due to error with FITS",
+                                array('FITS return value' => $return_var, 'OBJ file' => $path_to_obj));
+                        }
+                        else {
+                            $info_log->addInfo("FITS output generated",
+                                array('OBJ file' => $path_to_obj, 'FITS output' => $path_to_fits_output));
+                        }
+                    }
+                }
+                else {
+                    $error_log->addWarning("FITS output not generated because OBJ file not found",
+                        array('OBJ file' => $path_to_obj));
+                }
+            }
+        }
+    }
 }
 
 function generate_fits_for_book_pages($config, $info_log, $error_log, $path_to_fits, $obj_filename, $fits_output_filename, $record_key, $children_record_keys) {
-	if (count($children_record_keys)) {
-		foreach ($children_record_keys as $child_record_key) {
-			$path_to_book_dir = $config['WRITER']['output_directory'] . DIRECTORY_SEPARATOR . $record_key;
-			$page_dirs = glob($path_to_book_dir . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
-			foreach ($page_dirs as $path_to_page_dir) {
-				$path_to_obj = $path_to_page_dir . DIRECTORY_SEPARATOR . $obj_filename;
-				if (file_exists($path_to_obj)) {
-					$path_to_fits_output = $path_to_page_dir . DIRECTORY_SEPARATOR . $fits_output_filename;
-					if (!file_exists($path_to_fits_output)) {
-						$cmd = "$path_to_fits -i $path_to_obj -xc -o $path_to_fits_output";
-						exec($cmd, $output, $return_var);
-						if ($return_var) {
-							$error_log->addWarning("FITS output not generated due to error with FITS",
-								array('FITS return value' => $return_var, 'OBJ file' => $path_to_obj));
-						}
-						else {
-							$info_log->addInfo("FITS output generated",
-								array('OBJ file' => $path_to_obj, 'FITS output' => $path_to_fits_output));
-						}
-					}
-				}
-				else {
-					$error_log->addWarning("FITS output not generated because OBJ file not found",
-						array('OBJ file' => $path_to_obj));
-				}
-			}
-		}
-	}
+    if (count($children_record_keys)) {
+        foreach ($children_record_keys as $child_record_key) {
+            $path_to_book_dir = $config['WRITER']['output_directory'] . DIRECTORY_SEPARATOR . $record_key;
+            $page_dirs = glob($path_to_book_dir . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
+            foreach ($page_dirs as $path_to_page_dir) {
+                $path_to_obj = $path_to_page_dir . DIRECTORY_SEPARATOR . $obj_filename;
+                if (file_exists($path_to_obj)) {
+                    $path_to_fits_output = $path_to_page_dir . DIRECTORY_SEPARATOR . $fits_output_filename;
+                    if (!file_exists($path_to_fits_output)) {
+                        $cmd = "$path_to_fits -i $path_to_obj -xc -o $path_to_fits_output";
+                        exec($cmd, $output, $return_var);
+                        if ($return_var) {
+                            $error_log->addWarning("FITS output not generated due to error with FITS",
+                                array('FITS return value' => $return_var, 'OBJ file' => $path_to_obj));
+                        }
+                        else {
+                            $info_log->addInfo("FITS output generated",
+                                array('OBJ file' => $path_to_obj, 'FITS output' => $path_to_fits_output));
+                        }
+                    }
+                }
+                else {
+                    $error_log->addWarning("FITS output not generated because OBJ file not found",
+                        array('OBJ file' => $path_to_obj));
+                }
+            }
+        }
+    }
 }
 
 /**
