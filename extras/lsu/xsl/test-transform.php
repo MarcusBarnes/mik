9c47c9f8017b22066856ccba50c5575e2eeb29af
Jason Peak <jpeak5@lsu.edu>
2016-04-19 19:00:20 -0500

    never intended for commit

--- extras/lsu/xsl/test-transform.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function transform($mods, $xsl, $out){
-    exec("java -jar saxon9he.jar -s:$mods -xsl:$xsl  -o:$out");
-}
-
-transform("SampleInput/LSU_JJA-sample.xml", "subjectLSU_JJA.xsl", "testoutput.xml");
-
