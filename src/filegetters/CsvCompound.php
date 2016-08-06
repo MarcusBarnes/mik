3dbba1d9d969db326e74e0995eedde7138458dda
Mark Jordan <mjordan@sfu.ca>
2016-08-06 14:48:32 -0700

    Work on #234.

---
+++ src/filegetters/CsvCompound.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace mik\filegetters;
+
+class CsvCompound extends FileGetter
+{
+    /**
+     * @var array $settings - configuration settings from confugration class.
+     */
+    public $settings;
+
+    /**
+     * Create a new CSV Fetcher instance.
+     *
+     * @param array $settings configuration settings.
+     */
+    public function __construct($settings)
+    {
+        $this->settings = $settings['FILE_GETTER'];
+        $this->input_directory = $this->settings['input_directory'];
+        $this->cpd_directory_field = $this->settings['cpd_directory_field'];
+        $this->fetcher = new \mik\fetchers\Csv($settings);
+    }
+
+    /**
+     * Return a list of absolute filepaths to the pages of an issue.
+     *
+     * @param string $record_key
+     *
+     * @return array
+     *    An array of absolute paths to the object's child files.
+     */
+    public function getChildren($record_key)
+    {
+        $children_paths = array();
+        $cpd_input_path = $this->getCpdSourcePath($record_key);
+        foreach (glob($cpd_input_path . DIRECTORY_SEPARATOR . "*") as $path) {
+            $children_paths[] = $path;
+        }
+        return $children_paths;
+    }
+
+    /**
+     * Return the absolute filepath to the compound object directory.
+     *
+     * @param $record_key
+     *
+     * @return string
+     *    The absolute path to the compound object directory.
+     */
+    public function getCpdSourcePath($record_key)
+    {
+        // Get the path to the issue.
+        $item_info = $this->fetcher->getItemInfo($record_key);
+        $cpd_input_directory = $this->input_directory . DIRECTORY_SEPARATOR .
+            $item_info->{$this->cpd_directory_field};
+        return $cpd_input_directory;
+    }
+
+}
---
+++ src/writers/CsvCompound.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace mik\writers;
+use Monolog\Logger;
+
+class CsvCompound extends Writer
+{
+    /**
+     * @var array $settings - configuration settings from confugration class.
+     */
+    public $settings;
+
+    /**
+     * @var object $fetcher - fetcher class for item info methods.
+     */
+    private $fetcher;
+
+    /**
+     * @var object cdmPhpDocumentsFileGetter - filegetter class for
+     * getting files related to CDM PHP documents.
+     */
+    private $fileGetter;
+
+    /**
+     * Create a new newspaper writer instance
+     * @param array $settings configuration settings.
+     */
+    public function __construct($settings)
+    {
+        parent::__construct($settings);
+        $this->fetcher = new \mik\fetchers\Cdm($settings);
+        $fileGetterClass = 'mik\\filegetters\\' . $settings['FILE_GETTER']['class'];
+        $this->fileGetter = new $fileGetterClass($settings);
+        $this->output_directory = $settings['WRITER']['output_directory'];
+        $this->metadata_filename = $settings['WRITER']['metadata_filename'];
+        // Default is to generate page-level MODS.xml files.
+        if (isset($settings['WRITER']['generate_child_modsxml'])) {
+            $this->generate_child_modsxml = $settings['WRITER']['generate_child_modsxml'];
+        }
+        else {
+            $this->generate_child_modsxml = true;
+        }
+
+        // Set up logger.
+        $this->pathToLog = $this->settings['LOGGING']['path_to_log'];
+        $this->log = new \Monolog\Logger('Writer');
+        $this->logStreamHandler = new \Monolog\Handler\StreamHandler($this->pathToLog,
+            Logger::INFO);
+        $this->log->pushHandler($this->logStreamHandler);
+    }
+
+    /**
+     * Write folders and files.
+     *
+     * @param $metadata
+     * @param $pages
+     * @param $record_id
+     */
+    public function writePackages($metadata, $children, $record_id)
+    {
+        // If there were no datastreams explicitly set in the configuration,
+        // set flag so that all datastreams in the writer class are run.
+        // $this->datastreams is an empty array by default.
+        $no_datastreams_setting_flag = false;
+        if (count($this->datastreams) == 0) {
+            $no_datastreams_setting_flag = true;
+        }
+
+        // Create a compound-level subdirectory in the output directory to correspond
+        // to a compound-evel input subdirectory
+        $cpd_input_dir = $this->fileGetter->getCpdSourcePath($record_id);
+        if (file_exists($cpd_input_dir)) {
+            $cpd_output_dir = $this->output_directory . DIRECTORY_SEPARATOR . $record_id;
+            if (!file_exists($cpd_output_dir)) {
+                mkdir($cpd_output_dir);
+            }
+        }
+
+        $MODS_expected = in_array('MODS', $this->datastreams);
+        if ($MODS_expected xor $no_datastreams_setting_flag) {
+            $metadata_file_path = $cpd_output_dir . DIRECTORY_SEPARATOR . $this->metadata_filename;
+            $this->writeMetadataFile($metadata, $metadata_file_path);
+        }
+
+        $children_paths = $this->fileGetter->getChildren($record_id);
+        // @todo: Add error handling on mkdir and copy.
+        foreach ($children_paths as $child_path) {
+            $pathinfo = pathinfo($child_path);
+            if (preg_match('/thumbs\.db/i', $pathinfo['basename'])) {
+                continue;
+            }
+            // Get the sequence number from the filename. It is the last segment of the
+            // child filename, split on '_'.
+            $filename_segments = explode('_', $pathinfo['filename']);
+            $sequence_number = ltrim(end($filename_segments), '0');
+            $child_output_dir = $cpd_output_dir . DIRECTORY_SEPARATOR . $sequence_number;
+            mkdir($child_output_dir);
+
+            $OBJ_expected = in_array('OBJ', $this->datastreams);
+            if ($OBJ_expected xor $no_datastreams_setting_flag) {
+                $extension = $pathinfo['extension'];
+                $child_output_file_path = $child_output_dir . DIRECTORY_SEPARATOR . 'OBJ.' . $extension;
+                copy($child_path, $child_output_file_path);
+            }
+
+            if ($MODS_expected xor $no_datastreams_setting_flag) {
+                if ($this->generate_child_modsxml) {
+                    $this->writeChildMetadataFile($metadata, $sequence_number, $child_output_dir);
+                }
+            }
+        }
+    }
+
+    /**
+     * Writes out the child-level MODS.xml file.
+     *
+     * @param $metadata
+     *    The MODS XML document produced by the metadta parser.
+     * @param $path
+     *    The path to write the XML file to.
+     */
+    public function writeMetadataFile($metadata, $path)
+    {
+        // Add XML decleration
+        $doc = new \DomDocument('1.0');
+        $doc->loadXML($metadata);
+        $doc->formatOutput = true;
+        $metadata = $doc->saveXML();
+
+        if ($path !='') {
+            $fileCreationStatus = file_put_contents($path, $metadata);
+            if ($fileCreationStatus === false) {
+                $this->log->addWarning("There was a problem writing the issue-level metadata to a file",
+                    array('file' => $path));
+            }
+        }
+    }
+
+    /**
+     * Generates a very simple MODS.xml file for a newspaper page.
+     *
+     * @param $parent_metadata
+     *    The MODS document associated with the page's parent issue.
+     * @param $sequence_number
+     *    The child's sequence number, taken from its file's filename.
+     * @param $path
+     *    The path to write the XML file to.
+     */
+    public function writeChildMetadataFile($parent_metadata, $sequence_number, $path)
+    {
+        // Get the first title element from the issue's MODS.
+        $dom = new \DOMDocument;
+        $dom->loadXML($parent_metadata);
+        $xpath = new \DOMXPath($dom);
+        $titles = $xpath->query("//mods:titleInfo/mods:title");
+        $child_title = $titles->item(0)->nodeValue . ', part ' . $sequence_number;
+
+        $child_mods = <<<EOQ
+<mods xmlns="http://www.loc.gov/mods/v3" xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <titleInfo>
+    <title>{$child_title}</title>
+  </titleInfo>
+</mods>
+EOQ;
+        $path = $path . DIRECTORY_SEPARATOR . $this->metadata_filename;
+        $doc = new \DomDocument('1.0');
+        $doc->loadXML($child_mods);
+        $doc->formatOutput = true;
+        $metadata = $doc->saveXML();
+
+        if ($path !='') {
+            $fileCreationStatus = file_put_contents($path, $metadata);
+            if ($fileCreationStatus === false) {
+                $this->log->addWarning("There was a problem writing the child-level metadata to a file",
+                    array('file' => $path));
+            }
+        }
+    }
+
+}
