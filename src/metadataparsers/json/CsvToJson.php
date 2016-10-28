fe8a0e139d4b76b27e0596a4c8ee7531f553387c
Mark Jordan <mjordan@sfu.ca>
2016-10-27 21:47:45 -0700

    Work on #260.

---
+++ src/metadataparsers/json/CsvToJson.php
@@ -0,0 +1,109 @@
+<?php
+// src/metadataparsers/json/CsvToJson.php
+
+/**
+ * Example metadata parser class to demonstrate how to create something other
+ * than MODS or DC metadata.
+ *
+ * Doesn't support metadata manipulators, since it's just a demonstration.
+ */
+
+namespace mik\metadataparsers\json;
+
+use League\Csv\Reader;
+
+class CsvToJson extends Json
+{
+    /**
+     * @var array $metadatamanipulators - array of metadatamanimpulors from config.
+     */
+    public $metadatamanipulators;
+    
+    /**
+     * Create a new Metadata Instance
+     *
+     * @param $settings
+     */
+    public function __construct($settings)
+    {
+
+        parent::__construct($settings);
+
+        $this->fetcher = new \mik\fetchers\Csv($settings);
+        $this->record_key = $this->fetcher->record_key;
+
+        if (isset($this->settings['MANIPULATORS']['metadatamanipulators'])) {
+            $this->metadatamanipulators = $this->settings['MANIPULATORS']['metadatamanipulators'];
+        } else {
+            $this->metadatamanipulators = null;
+        }
+    }
+
+    /**
+     * Converts the object's metadata into JSON.
+     *
+     * @param object $objectInfo
+     *   The object's metadata.
+     *
+     * @return string
+     *   The serialized JSON version of the object's metadata.
+     */
+    public function createJson($objectInfo)
+    {
+        $record_key_column = $this->record_key;
+        $record_key = $objectInfo->$record_key_column;
+
+        foreach ($objectInfo as $label => &$value) {
+            if (isset($this->metadatamanipulators)) {
+                $value = $this->applyMetadatamanipulators($value, $record_key, $label);
+            }
+        }
+
+        $json = json_encode($objectInfo);
+        return $json;
+    }
+
+    /**
+     * Gets the JSON metadata for a specific object.
+     *
+     * @param string $record_key
+     *   The object's record key.
+     *
+     * @return string
+     *   The serialized JSON version of the object's metadata.
+     */
+    public function metadata($record_key)
+    {
+        $objectInfo = $this->fetcher->getItemInfo($record_key);
+        $metadata = $this->createJson($objectInfo);
+        return $metadata;
+    }
+
+    /**
+     * Applies metadatamanipulators listed in the config to provided XML snippet.
+     *
+     * @param string $value
+     *     The CSV field value.
+     * @param string $record_key
+     *     The item's record key value.
+     * @param string $field_name
+     *     The CSV field name.
+     *
+     * @return string
+     *     The field value after all manipulators have been applied to it.
+     */
+    private function applyMetadatamanipulators($value, $record_key, $field_name)
+    {
+        foreach ($this->metadatamanipulators as $metadatamanipulator) {
+            $metadatamanipulatorClassAndParams = explode('|', $metadatamanipulator);
+            $metadatamanipulatorClassName = array_shift($metadatamanipulatorClassAndParams);
+            $manipulatorParams = $metadatamanipulatorClassAndParams;
+            $metdataManipulatorClass = 'mik\\metadatamanipulators\\' . $metadatamanipulatorClassName;
+            $metadatamanipulator = new $metdataManipulatorClass($this->settings, $manipulatorParams,  $record_key, $field_name);
+            $value = $metadatamanipulator->manipulate($value);
+        }
+
+        return $value;
+    }
+
+}
