925c5a01a9c3f1dabf3f4af96509e0f4aa79ebf0
Mark Jordan <mjordan@sfu.ca>
2017-03-25 08:07:26 -0700

    Work on #359.

--- src/filegetters/OaipmhIslandoraObj.php
+++ src/filegetters/OaipmhIslandoraObj.php
@@ -63,42 +63,34 @@ class OaipmhIslandoraObj extends FileGetter
         $dom = new \DOMDocument;
         $xml = file_get_contents($raw_metadata_path);
         $dom->loadXML($xml);
-        // Loop through all the dc:identifer elements.
-        foreach ($dom->getElementsByTagNameNS('http://purl.org/dc/elements/1.1/', 'identifier') as $identifier) {
-            // This scary looking regex pattern identifies a valid Fedora PID.
-            if (preg_match('/^([A-Za-z0-9]|-|\.)+:(([A-Za-z0-9])|-|\.|~|_|(%[0-9A-F]{2}))+$/', trim($identifier->nodeValue))) {
-                $pid = trim($identifier->nodeValue);
-                $islandora_url_info = parse_url($this->oai_endpoint);
-                if (isset($islandora_url_info['port'])) {
-                    $port = $islandora_url_info['port'];
+        // There will only be one oai:identifer element. Islandora's OAI identifiers look like
+        // oai:digital.lib.sfu.ca:foo_112, 'foo_123' being the object's PID.
+        $identifier = $dom->getElementsByTagNameNS('http://www.openarchives.org/OAI/2.0/', 'identifier')->item(0);
+        $raw_pid = preg_replace('#.*:#', '', trim($identifier->nodeValue));
+        $pid = preg_replace('/_/', ':', $raw_pid);
+            $islandora_url_info = parse_url($this->oai_endpoint);
+            if (isset($islandora_url_info['port'])) {
+                $port = $islandora_url_info['port'];
+            }
+            else {
+                $port = '';
+            }
+            $islandora_host = $islandora_url_info['scheme'] . '://' . $islandora_url_info['host'] . $port;
+            // Assemble the URL of each datastream listed in the config and return on the first one
+            // that is available. We loop through DSIDs because not all Islandora content models
+            // require an OBJ datastream, e.g., PDF, video and audio content models.
+            foreach ($this->datastreamIds as $dsid) {
+                $ds_url = $islandora_host . '/islandora/object/' . $pid . '/datastream/' . $dsid . '/download';
+                // HEAD is probably more efficient than the default GET.
+                stream_context_set_default(array('http' => array('method' => 'HEAD')));
+                $headers = get_headers($ds_url, 1);
+                if ($headers[0] == 'HTTP/1.1 200 OK') {
+                    return $ds_url;
                 }
-                else {
-                    $port = '';
-                }
-                $islandora_host = $islandora_url_info['scheme'] . '://' . $islandora_url_info['host'] . $port;
-                // Assemble the URL of each datastream listed in the config and return on the first one
-                // that is available. We loop through DSIDs because not all Islandora content models
-                // require an OBJ datastream, e.g., PDF, video and audio content models.
-                foreach ($this->datastreamIds as $dsid) {
-                    $ds_url = $islandora_host . '/islandora/object/' . $pid . '/datastream/' . $dsid . '/download';
-                    // HEAD is probably more efficient than the default GET.
-                    stream_context_set_default(
-                        array(
-                            'http' => array(
-                                'method' => 'HEAD'
-                             )
-                        )
-                    );
-                    $headers = get_headers($ds_url, 1);
-                    if ($headers[0] == 'HTTP/1.1 200 OK') {
-                        return $ds_url;
-                    }
-                }
-                // If no datastreams are available, return false.
-                return false;
-             }
-         }
-         // If no dc:identifiers contain what appears to be a PID (unlikely, but possible), return false.
-         return false;
+            }
+            // If no datastreams listed in $this->datastreamIds are available, return false.
+            return false;
+        // If no oai:identifiers contain what appears to be a PID (unlikely, but possible), return false.
+        return false;
     }
 }
