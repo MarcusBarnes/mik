380e0de49c57b6a26a497a9dfc97829d1c696357
Mark Jordan <mjordan@sfu.ca>
2017-03-26 16:57:44 -0700

    Work on #361.

--- src/metadatamanipulators/InsertXmlFromTemplate.php
+++ src/metadatamanipulators/InsertXmlFromTemplate.php
@@ -52,6 +52,8 @@ class InsertXmlFromTemplate extends MetadataManipulator
          if ($this->fieldName == $this->sourceField) {
              $loader = new \Twig_Loader_Filesystem($this->templateDirectory);
              $twig = new \Twig_Environment($loader);
+             $custom_filter = new \Twig_SimpleFilter('TwigTruncate', array($this, 'TwigTruncate'));
+             $twig->addFilter($custom_filter);
              $metadata = $this->getSourceMetadata();
              $xml_from_template = $twig->render($this->templateFilename, (array) $metadata);
              return trim($xml_from_template);
@@ -102,4 +104,38 @@ class InsertXmlFromTemplate extends MetadataManipulator
           );
       }
 
+    /**
+     * Custom Twig filter to truncate a string to a specific length.
+     *
+     * Will truncate at the prceding word boundary, which means that
+     * the returned string may be empty.
+     *
+     * Example: <title>{{ Title|TwigTruncate(20) }} [...]</title>
+     *
+     * @param string $string
+     *   The string to truncate.
+     * @param int $length
+     *   The length at which to truncate the string.
+     *
+     * @return string
+     *   The truncated string. If the wordsafe string has been truncated
+     *   to be '', the original string is returned instead.  
+     */
+     public function TwigTruncate($string, $length)
+     {
+         // First truncate the string.
+         $truncated = substr($string, 0, $length);
+         // Then determine what the nearest preceding word boundary is,
+         // tokenizing the words on ' '.
+         $tokenized_words = explode(' ', $truncated);
+         array_pop($tokenized_words);
+         $wordsafe_string = implode(' ', $tokenized_words);
+
+         if (strlen($wordsafe_string) === 0) {
+             return $string;
+         }
+         else {
+             return $wordsafe_string;
+         }
+     }
 }
