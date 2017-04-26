26e45ac7e268f7513968ed011b7eef0d2f7fbf06
Mark Jordan <mjordan@sfu.ca>
2017-04-25 19:36:01 -0700

    Work on #379.

--- src/metadatamanipulators/InsertXmlFromTemplate.php
+++ src/metadatamanipulators/InsertXmlFromTemplate.php
@@ -2,6 +2,7 @@
 // src/metadatamanipulators/InsertXmlFromTemplate.php
 
 namespace mik\metadatamanipulators;
+
 use \Twig\Twig;
 use \Monolog\Logger;
 
@@ -52,8 +53,10 @@ class InsertXmlFromTemplate extends MetadataManipulator
          if ($this->fieldName == $this->sourceField) {
              $loader = new \Twig_Loader_Filesystem($this->templateDirectory);
              $twig = new \Twig_Environment($loader);
-             $custom_filter = new \Twig_SimpleFilter('TwigTruncate', array($this, 'TwigTruncate'));
-             $twig->addFilter($custom_filter);
+
+             $truncate_filter = new \Twig_SimpleFilter('TwigTruncate', 'mik\utilities\MikTwigExtension::TwigTruncate');
+             $twig->addFilter($truncate_filter);
+
              $metadata = $this->getSourceMetadata();
              $xml_from_template = $twig->render($this->templateFilename, (array) $metadata);
              return trim($xml_from_template);
@@ -103,39 +106,4 @@ class InsertXmlFromTemplate extends MetadataManipulator
               'Unrecognized fetcher class' => $this->settings['FETCHER']['class'])
           );
       }
-
-    /**
-     * Custom Twig filter to truncate a string to a specific length.
-     *
-     * Will truncate at the prceding word boundary, which means that
-     * the returned string may be empty.
-     *
-     * Example: <title>{{ Title|TwigTruncate(20) }} [...]</title>
-     *
-     * @param string $string
-     *   The string to truncate.
-     * @param int $length
-     *   The length at which to truncate the string.
-     *
-     * @return string
-     *   The truncated string. If the wordsafe string has been truncated
-     *   to be '', the original string is returned instead.  
-     */
-     public function TwigTruncate($string, $length)
-     {
-         // First truncate the string.
-         $truncated = substr($string, 0, $length);
-         // Then determine what the nearest preceding word boundary is,
-         // tokenizing the words on ' '.
-         $tokenized_words = explode(' ', $truncated);
-         array_pop($tokenized_words);
-         $wordsafe_string = implode(' ', $tokenized_words);
-
-         if (strlen($wordsafe_string) === 0) {
-             return $string;
-         }
-         else {
-             return $wordsafe_string;
-         }
-     }
 }
