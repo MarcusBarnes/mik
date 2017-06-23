bb035888f404781f112a23d077a4866e0d67a3ec
Mark Jordan <mjordan@sfu.ca>
2017-06-23 15:23:03 -0700

    Work on #40 and #365.

--- src/metadataparsers/mods/Mods.php
+++ src/metadataparsers/mods/Mods.php
@@ -33,18 +33,25 @@ abstract class Mods extends MetadataParser
     }
 
     /**
-     *  Create MODS XML
-     *  @param array $colletionMappyingArray collection mappings
-     *  @param array $objectInfo array of info about the object that the MODS XML will be created for
+     *  Create MODS XML.
+     *
+     *  @param array $colletionMappyingArray
+     *    Collection mappings
+     *  @param array $objectInfo
+     *    Array of info about the object that the MODS XML will be created for
      */
     abstract public function createModsXML($collectionMappingArray, $objectInfo);
 
+    /**
+     *  Writes out the serialized MODS XML file.
+     *
+     *  @param string $modsxml
+     *     A MODS XML string.
+     *  @param string $outputPath
+     *     Destination path for the output file.
+     */
     public function outputModsXML($modsxml, $outputPath = '')
     {
-        /**
-         * $modsxml - MODS xml string - required.
-         * $outputPath - output path for writing to a file.
-         */
         if ($outputPath !='') {
             $filecreationStatus = file_put_contents($outputPath .'/MODS.xml', $modsxml);
             if ($filecreationStatus === false) {
@@ -71,7 +78,6 @@ abstract class Mods extends MetadataParser
         $childNodesElementSignatureStringArray = array();
         foreach ($xml->documentElement->childNodes as $node) {
             $elementName = $node->nodeName;
-
             $elementAttributesMap = array();
             $attributesNodeMap = $node->attributes;
             $len = $attributesNodeMap->length;
@@ -82,12 +88,10 @@ abstract class Mods extends MetadataParser
                 $elementAttributesMap[$attributeName] = $attributeValue;
             }
             $elementSignature = array($elementName, $elementAttributesMap);
-
             $childNodesElementSignatureArray[] = $this->signatureToString($elementSignature);
         }
 
         $returnArray = array_unique($childNodesElementSignatureArray);
-
         return $returnArray;
     }
 
@@ -95,20 +99,22 @@ abstract class Mods extends MetadataParser
      * Determine which child elements of MODS root element are wrapper elements:
      *  1) They have child elements of type XML_ELEMENT_NODE (Value: 1)
      *
-     * @param $xml object MODS XML object
+     * @param $xml object
+     *   MODS XML object
      *
-     * @param $uniqueChildNodeSignatureArray an array that lists the unique element
-     * signatures of children of the root MOD element.
+     * @param $uniqueChildNodeSignatureArray
+     *   An array that lists the unique element
+     *   signatures of children of the root MOD element.
      *
-     * @return arrry of XML elemets that are wrapper elements (children of root element).
+     * @return array of XML elemets that are wrapper elements (children of root element).
      */
     public function determineRepeatedWrapperChildElements($xml, $uniqueChildNodeSignatureArray)
     {
         $wrapperDomNodes = array();
-        // Grab the elements
+        // Grab the elements.
         foreach ($uniqueChildNodeSignatureArray as $nodeSignatureString) {
 
-            // Turn node signature string into parts element name + element attributes
+            // Turn node signature string into parts element name + element attributes.
             $nodeSignature = $this->elementSignatureFromString($nodeSignatureString);
 
             $nodeName = $nodeSignature[0];
@@ -116,7 +122,6 @@ abstract class Mods extends MetadataParser
             //DOMNodeList
             $nodeListObj = $xml->getElementsByTagName($nodeName);
             if ($nodeListObj->length >= 2 && !in_array($nodeName, $this->repeatableWrapperElements)) {
-
                 // The element name appears more than once and
                 // is not set as an allowed repeatable wrapper element.
                 foreach ($nodeListObj as $node) {
@@ -138,9 +143,11 @@ abstract class Mods extends MetadataParser
      * If a (parent) wrapper element more than once, consolidate the child nodes
      * into only one parent wrapper element and return the consolidated elements.
      *
-     * @param array $wrapperElementArray An array of wrapper elements.
+     * @param array $wrapperElementArray
+     *   An array of wrapper elements.
      *
-     * @return array An array of consolidated wrapper elements of type XML_ELEMENT_NODE
+     * @return array
+     *   An array of consolidated wrapper elements of type XML_ELEMENT_NODE
      */
     public function consolidateWrapperElements($wrapperElementArray)
     {
@@ -167,7 +174,7 @@ abstract class Mods extends MetadataParser
                 $attributeValue = $attributeItem->value;
                 $elementAttributesMap[$attributeName] = $attributeValue;
             }
-            // dev - rather than just check element name, check element name
+            // Rather than just check element name, check element name
             // and element attributes.
             $elementSignature = array($elementName, $elementAttributesMap);
 
@@ -179,7 +186,6 @@ abstract class Mods extends MetadataParser
             } else {
                 // $elementSignature is not in the tracking array, add it.
                 array_push($elementSignatureTrackingArray, $elementSignature);
-
                 $keyFromSignature = $this->signatureToString($elementSignature);
                  // $elementName is not in the tracking array, add it.
                 $elementNameTrackingArray[$keyFromSignature] = array($wrapperElement->childNodes);
@@ -222,7 +228,6 @@ abstract class Mods extends MetadataParser
      */
     public function oneParentWrapperElement($xmlString)
     {
-
         $xml = new \DomDocument();
         $xml->loadXML($xmlString);
 
@@ -287,9 +292,9 @@ abstract class Mods extends MetadataParser
             $elementName = $elementNameAttributesArray[0];
             $elementAttributeKeyValueArray = array();
             if(count($elementNameAttributesArray) > 1){
-                // element has attributes.
+                // Element has attributes.
                 $elementAttributesArray = explode('|', $elementNameAttributesArray[1]);
-                // remove empty, false, or null values from the array.
+                // Remove empty, false, or null values from the array.
                 $elementAttributesArray = array_filter($elementAttributesArray);
                 foreach($elementAttributesArray as $attributeValue){
                     $attributeValuePair = explode('=', $attributeValue);
